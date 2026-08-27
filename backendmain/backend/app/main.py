627999acb534e388562d32d3778f0ed9c9a287f7
HafizArslan2003 <arslanashraf0808@gmail.com>
2026-08-27 13:01:41 +0500

    test

--- backendmain/backend/app/main.py
+++ backendmain/backend/app/main.py
@@ -24,13 +24,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app",
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:3000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
